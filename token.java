f39c127ff706057566529c5ce26b60b403fa9a23
Felipe Monsalve Cantin <fimonsalve@gmail.com>
2017-11-24 22:38:06 -0300

    Se agrega la clase Token

---
+++ token.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+import java.util.*;
+
+public class token implements Serializable{
+  Vector<Integer> tokenv = new Vector<Integer>();
+  Queue<Integer> tokenq = new LinkedList<Integer>();
+  
+}
